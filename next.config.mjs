fc223e149a604d3d9d862ffa4c0fce16337632f7
Cognition X Lab <info.cognitionx@gmail.com>
2026-07-09 20:11:24 +0800

    Integrate with Content Collections in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,5 @@
+import { withContentCollections } from '@content-collections/next';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
@@ -9,4 +11,5 @@ const nextConfig = {
   // basePath: '/repository-name', 
 };
 
-export default nextConfig;
+export default withContentCollections(nextConfig);
+
